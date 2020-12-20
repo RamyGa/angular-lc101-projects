40d2d8e1cc68b1409d80d5e83b385f2d8f907fbe
Ramyga <research@ronolabs.com>
2020-12-20 15:09:17 -0600

    added images and text

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Pix';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.pinimg.com/236x/52/f3/0a/52f30a5ef6e3c60b86c0fb6da9326a1a--anime-dragon-anime-hot.jpg';
+  image3 = 'https://i.pinimg.com/originals/60/6e/15/606e15ffb471efa3ed15b15b9d14b03e.jpg';
 
   constructor() { }
 
